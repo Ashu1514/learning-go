f1095dc312ab6dcd469f1253b14c9b5b86191ec3
Ashu Londhe <connect@altevio.com>
2026-04-19 19:27:06 +0200

    basic go: custom functions

--- investment-calculator/app.go
+++ investment-calculator/app.go
@@ -11,18 +11,22 @@ func main(){
 	expectedReturnRate := 7.5
 	// var years float64 = 10
 
-	fmt.Print("Enter a Investment Amount: ")
+	outputText("Enter a Investment Amount: ")
 	fmt.Scan(&investmentAmount)
 
-	fmt.Print("Enter a Investment Years: ")
+	outputText("Enter a Investment Years: ")
 	fmt.Scan(&years)
 
-	fmt.Print("Enter a Expected Return Rate: ")
+	outputText("Enter a Expected Return Rate: ")
 	fmt.Scan(&expectedReturnRate)
 
 	var futureValue = investmentAmount * math.Pow(1 + (expectedReturnRate / 100), years)
 	futureRealValue := futureValue / math.Pow(1 + (inflationRate / 100), years)
 
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+	fmt.Printf("Future value is %.2f\n",futureValue)
+	fmt.Printf("Future value with inflation %.2f\n", futureRealValue)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
 }
