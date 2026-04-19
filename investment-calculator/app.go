2bbb3d444848cad1263269a452c69f15ca96d28c
Ashu Londhe <connect@altevio.com>
2026-04-19 19:55:14 +0200

    basic go: function return type

--- investment-calculator/app.go
+++ investment-calculator/app.go
@@ -5,8 +5,9 @@ import (
 	"math"
 )
 
+const inflationRate float64 = 2.5
+
 func main(){
-	const inflationRate float64 = 2.5
 	var investmentAmount, years float64
 	expectedReturnRate := 7.5
 	// var years float64 = 10
@@ -20,8 +21,7 @@ func main(){
 	outputText("Enter a Expected Return Rate: ")
 	fmt.Scan(&expectedReturnRate)
 
-	var futureValue = investmentAmount * math.Pow(1 + (expectedReturnRate / 100), years)
-	futureRealValue := futureValue / math.Pow(1 + (inflationRate / 100), years)
+	futureValue, futureRealValue := calculateFuturevalue(investmentAmount, expectedReturnRate, years)
 
 	fmt.Printf("Future value is %.2f\n",futureValue)
 	fmt.Printf("Future value with inflation %.2f\n", futureRealValue)
@@ -29,4 +29,11 @@ func main(){
 
 func outputText(text string) {
 	fmt.Print(text)
+}
+
+func calculateFuturevalue(investmentAmount, expectedReturnRate, years float64) (fv float64, rfv float64) {
+	fv = investmentAmount * math.Pow(1 + (expectedReturnRate / 100), years)
+	rfv = fv / math.Pow(1 + (inflationRate / 100), years)
+
+	return fv, rfv
 }
--- profit-tax-calculator/app.go
+++ profit-tax-calculator/app.go
@@ -3,21 +3,27 @@ package main
 import "fmt"
 
 func main(){
-	var revenue, expenses, taxRate float64
+	revenue := getUserInput("Revnue: ")
+	expenses := getUserInput("Expenses: ")
+	taxRate := getUserInput("Tax Rate: ")
 
-	fmt.Print("Revnue: ")
-	fmt.Scan(&revenue)
+	ebt, profit, ratio := calculateProfit(revenue, expenses, taxRate)
 
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
+	fmt.Println("EBT: ", ebt)
+	fmt.Println("Profit: ", profit)
+	fmt.Printf("Ratio: %.2f", ratio)
+}
 
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
+func getUserInput(infoText string) float64 {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+	return userInput
+}
 
-	ebt := (revenue - expenses)
-
-	profit := ebt * (1 - (taxRate / 100))
-
-	println("EBT: ", ebt)
-	println("Profit: ", profit)
+func calculateProfit(revenue, expenses, taxRate float64) (ebt, profit, ratio float64) {
+	ebt = (revenue - expenses)
+	profit = ebt * (1 - (taxRate / 100))	
+	ratio = ebt / profit
+	return ebt, profit, ratio
 }
