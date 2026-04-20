badec7bfee29c737da5f58b6a18f2111ac6298b9
Ashu Londhe <ashulondhe50@gmail.com>
2026-04-20 19:47:53 +0200

    working bank app with if-else control flows

---
+++ bank/app.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+func main() {
+	var accountBalance = 1000.0
+
+	fmt.Println("Welcome to Go Bank!")
+	fmt.Println("What do you want to do?")
+	fmt.Println("1. Check Balance")
+	fmt.Println("2. Deposit Money")
+	fmt.Println("3. Withdraw Money")
+	fmt.Println("4. Exit")
+
+	var choice int
+
+	fmt.Print("Your Choice: ")
+	fmt.Scan(&choice)
+
+	if choice == 1 {
+		fmt.Println("Your Balance is", accountBalance)
+	} else if choice == 2 {
+		fmt.Println("How much do you want to deposit?")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+		if depositAmount <= 0 {
+			fmt.Println("Invalid Amount! Must be greater than 0")
+			return
+		}
+		accountBalance += depositAmount
+		fmt.Println("Balance updated! New Amount:", accountBalance)
+	} else if choice == 3 {
+		fmt.Println("How much do you want to withdraw?")
+		var withdrawalAmount float64
+		fmt.Scan(&withdrawalAmount)
+
+		if withdrawalAmount <= 0 {
+			fmt.Println("Invalid Amount! Must be greater than 0")
+			return
+		} 
+		
+		if withdrawalAmount > accountBalance {
+			fmt.Println("Insufficiant Balance!")
+			return
+		}
+
+		accountBalance -= withdrawalAmount
+		fmt.Println("Balance updated! New Amount:", accountBalance)
+	} else {
+		fmt.Println("Goodbye!")
+	}
+
+}
