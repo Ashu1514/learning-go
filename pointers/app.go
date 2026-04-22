4de12bfa2de27cecd5e8f58c6aad7c03543da38f
Ashu Londhe <ashulondhe50@gmail.com>
2026-04-22 21:00:54 +0200

    learning pointer: manipulated value using pointer

--- pointers/app.go
+++ pointers/app.go
@@ -3,20 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	age := 32
+	age := 32 // regular variable
 
 	// var agePointer *int
-	agePointer := &age
+	agePointer := &age // pointer variable
 
 	fmt.Println("Age:", age)
 	fmt.Println("Age Pointer:", agePointer)
 	fmt.Println("Age Pointer value:", *agePointer)
 
 
-	fmt.Println(getAdultYears(agePointer)) // pasing pointer means address of value
-	// fmt.Println(getAdultYears(&age)) // this also does the same
+	// fmt.Println(getAdultYears(agePointer)) // pasing pointer means address of value
+	// fmt.Println(getAdultYears(&age)) // this also does the same as above func calling
+
+	editAgeToAdultYears(agePointer) // manipulated value in function and stored at same address agian using pointers
+	fmt.Println("Adult Age:", age) // printing original variable
 }
 
-func getAdultYears(age *int) int {
-	return *age - 18 // *age to get value at pointer address
+func editAgeToAdultYears(age *int) {
+	// return *age - 18 // *age to get value at pointer address
+	*age = *age - 18 // manipulating value at pointer and restore it at same pointer 
 }
